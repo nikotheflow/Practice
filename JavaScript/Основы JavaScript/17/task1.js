f6a33100f02211dea0a1d01d5a97fb480bf91bd4
nikotheflow <bboyfoxxx@yandex.ru>
2019-02-13 15:32:51 +0300

    Add task 17.1

---
+++ JavaScript/Основы JavaScript/17/task1.js
@@ -0,0 +1,18 @@
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    return confirm('Родители разрешили?');
+  }
+}
+
+
+//Используя оператор '?'
+function checkAge(age) {
+	return (age > 18) ? true : confirm('Родители разрешили?');
+}
+
+//Используя оператор '||'
+function checkAge(age) {
+	return (age > 18) || confirm('Родители разрешили?');
+}
