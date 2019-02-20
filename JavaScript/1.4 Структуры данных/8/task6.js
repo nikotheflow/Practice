182d5c6a34aa80e46036d9bbaa31e978273b97fb
nikotheflow <bboyfoxxx@yandex.ru>
2019-02-20 10:51:23 +0300

    Add task 8.6

---
+++ JavaScript/1.4 Структуры данных/8/task6.js
@@ -0,0 +1,6 @@
+var arr = ["HTML", "JavaScript", "CSS"];
+
+var arrSorted = arr.slice().sort();
+
+alert( arrSorted ); // CSS, HTML, JavaScript
+alert( arr ); // HTML, JavaScript, CSS (без изменений)
