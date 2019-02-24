a81d5e87a04e1595fc26d88b2e536afad3d0d2c0
nikotheflow <bboyfoxxx@yandex.ru>
2019-02-24 23:10:09 +0300

    Add task 2.5

---
+++ JavaScript/1.6 Методы объектов и контекст вызова/2/task5.js
@@ -0,0 +1,19 @@
+function sum(a) {
+  var currentSum = a;
+  
+  function f(b) {
+    currentSym += b;
+    return f;
+  }
+  
+  f.toString = function() {
+    return currentSum;
+  }
+  
+  return f;
+}
+
+alert( sum(1)(2) ); // 3
+alert( sum(5)(-1)(2) ); // 6
+alert( sum(6)(-1)(-2)(-3) ); // 0
+alert( sum(0)(1)(2)(3)(4)(5) ); // 15
