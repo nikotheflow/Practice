f792375e9b32cad2b2df7148fd955bbe6257bc93
nikotheflow <bboyfoxxx@yandex.ru>
2019-02-25 12:24:37 +0300

    Add task 3.3

---
+++ JavaScript/1.6 Методы объектов и контекст вызова/3/task3.js
@@ -0,0 +1,12 @@
+function Accumulator(startingValue) {
+  this.value = startingValue;
+  
+  this.read = function() {
+    this.value += +prompt('Число?', 0);
+  };
+}
+
+var accumulator = new Accumulator(1); // начальное значение 1
+accumulator.read(); // прибавит ввод prompt к текущему значению
+accumulator.read(); // прибавит ввод prompt к текущему значению
+alert( accumulator.value ); // выведет текущее значение
