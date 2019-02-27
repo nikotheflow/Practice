2d9122cbc3806c6d93af4bd3ef1d7b661eaf87d7
nikotheflow <bboyfoxxx@yandex.ru>
2019-02-28 00:15:38 +0300

    Add task 7.5

---
+++ JavaScript/1.6 Методы объектов и контекст вызова/7/task5.js
@@ -0,0 +1,22 @@
+"use strict";
+
+function ask(question, answer, ok, fail) {
+  var result = prompt(question, '');
+  if (result.toLowerCase() == answer.toLowerCase()) ok();
+  else fail();
+}
+
+var user = {
+  login: 'Василий',
+  password: '12345',
+
+  loginDone: function(result) {
+    alert( this.login + (result ? ' вошёл в сайт' : ' ошибка входа') );
+  },
+
+  checkPassword: function() {
+    ask("Ваш пароль?", this.password, this.loginDone.bind(this, true), this.loginDone.bind(this, false));
+  }
+};
+
+user.checkPassword();
